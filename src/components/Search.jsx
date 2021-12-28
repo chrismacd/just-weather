1e7c5ddf49904d91f7f76d93009ee26bdc3d49e4
Chris Macdonald <macdochris@gmail.com>
2021-12-28 17:45:38 +0000

    basic search logic

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const data = require('../data/city.list.min.json');
 
 function Search() {
-  const [input, setInput] = useState('');
+  const [text, setText] = useState('');
 
-  const onChange = (e) => {
-    setInput(e.target.value);
-  };
+  useEffect(() => {
+    console.log(text);
+
+    if (text.length >= 3) {
+      const regex = new RegExp(`^${text.toLowerCase()}`, 'g');
+      const results = data.filter((city) => {
+        return city.name.toLowerCase().search(regex) > -1;
+      });
+
+      console.log(results.sort((a, b) => (a.name < b.name ? -1 : 1)));
+    }
+  }, [text]);
 
   return (
     <div className='search'>
@@ -16,8 +27,8 @@ function Search() {
           name='s'
           id='s'
           className='border w-full max-w-xs h-10 px-3'
-          value={input}
-          onChange={(e) => onChange(e)}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
       </label>
     </div>
