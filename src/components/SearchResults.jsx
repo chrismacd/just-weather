3dcbbc4c94db7ac7e4bc60a84d99c2db958bb9b3
Chris Macdonald <macdochris@gmail.com>
2021-12-29 14:14:48 +0000

    added flag to search results

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,24 +1,31 @@
-function SearchResults({ cities }) {
-  console.log(cities);
+import Flags from 'country-flag-icons/react/3x2';
 
+function SearchResults({ cities }) {
   function onClick(id) {
     console.log(id);
   }
 
   return (
     <div className='results border'>
-      {cities.map((city) => (
-        <div className='px-2 py-1 hover:bg-lightblue' key={city.id}>
-          <button
-            className='text-sm'
-            type='button'
-            onClick={() => onClick(city.id)}
-          >
-            {city.name}
-            {city.state && `, ${city.state}`}
-          </button>
-        </div>
-      ))}
+      {cities.map((city) => {
+        const Flag = Flags[city.country];
+
+        return (
+          <div className='px-2 py-1 hover:bg-lightblue' key={city.id}>
+            <button
+              className='text-sm flex w-full items-center'
+              type='button'
+              onClick={() => onClick(city.id)}
+            >
+              <Flag className='w-4 mr-2' title={city.country} />
+              <span>
+                {city.name}
+                {city.state && `, ${city.state}`}
+              </span>
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
