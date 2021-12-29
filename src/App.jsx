be9b77342c62cb55b0259404364fa1d6f6df40cd
Chris Macdonald <macdochris@gmail.com>
2021-12-29 16:57:54 +0000

    removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import useFetchWeather from './hooks/useFetchWeather';
 import Search from './components/Search';
 import Details from './components/Details';
@@ -7,53 +7,12 @@ import './App.css';
 
 function App() {
   const [cityId, setCityId] = useState(0);
-  /*
-  const [cityWeather, setCityWeather] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-  */
 
   const handleChangeCity = (id) => {
     setCityId(id);
   };
 
   const { data: cityWeather } = useFetchWeather(cityId);
-
-  useEffect(() => {
-    console.log('city change', cityId);
-
-    /*
-    if (cityId > 0) {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=a61518b113182a0a4b4cc2468498c201`
-      )
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Could not fetch the data for that resource');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          const weather = {
-            name: data.name,
-            temperature: Math.round(Number(data.main.temp) - 273.15),
-            humidity: data.main.humidity,
-            windSpeed: data.wind.speed,
-          };
-
-          setCityWeather(weather);
-
-          console.log(data, isLoading, error);
-          setIsLoading(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsLoading(false);
-          setError(err.message);
-        });
-    }
-    */
-  }, [cityId]);
 
   return (
     <div className='container mx-auto px-6'>
