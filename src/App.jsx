4e3dccd2bc6fa5f0a30db6f66bc7de1a2bc07547
Chris Macdonald <macdochris@gmail.com>
2021-12-29 14:40:43 +0000

    added state for selected city

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import Search from './components/Search';
 import Details from './components/Details';
 import Favourites from './components/Favourites';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState(0);
+
+  const handleChangeCity = (id) => {
+    setCity(id);
+  };
+
   return (
     <div className='container mx-auto px-6'>
-      <Search />
-      <Details />
+      <Search handleChangeCity={handleChangeCity} />
+      {city > 0 && <Details />}
       <Favourites />
     </div>
   );
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,14 @@
-function Details() {
-  return <div />;
+function Details({ name, temperature, windSpeed, humidity }) {
+  return (
+    <div className='details'>
+      <h1>{name}</h1>
+      <ul>
+        <li>Temperature: {temperature}</li>
+        <li>Wind speed: {windSpeed}</li>
+        <li>Humidity: {humidity}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Details;
