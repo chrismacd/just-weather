97190d4108211b1c4dc469087d0a5b5d014b9e6c
Chris Macdonald <macdochris@gmail.com>
2021-12-30 00:16:21 +0000

    favourites list clickable

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,6 +1,8 @@
+import City from './City';
+
 const cityList = require('../data/city.list.min.json');
 
-function Favourites({ favourites }) {
+function Favourites({ favourites, handleChangeCity }) {
   return (
     favourites && (
       <div className='favourites'>
@@ -10,7 +12,16 @@ function Favourites({ favourites }) {
           {favourites.map((id) => {
             const city = cityList.find((item) => item.id === id);
 
-            return <li key={id}>{city.name}</li>;
+            return (
+              <City
+                id={city.id}
+                name={city.name}
+                state={city.state}
+                country={city.country}
+                handleChange={handleChangeCity}
+                key={city.id}
+              />
+            );
           })}
         </ul>
       </div>
