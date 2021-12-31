a29555ca46ca2af68c71f287bd57075e3226ff2e
Chris Macdonald <macdochris@gmail.com>
2021-12-31 13:20:10 +0000

    added test for search results list

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
 test('renders favourites heading', () => {
@@ -7,8 +7,35 @@ test('renders favourites heading', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('renders search input', () => {
-  render(<App />);
-  const input = screen.getByPlaceholderText(/search for a city/i);
-  expect(input).toBeInTheDocument();
+describe('tests search input', () => {
+  test('renders search input', () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText(/search for a city/i);
+    expect(input).toBeInTheDocument();
+  });
+
+  test('search result should be visible', async () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText(/search for a city/i);
+    fireEvent.change(input, { target: { value: 'joh' } });
+
+    await waitFor(() => {
+      expect(screen.getByText('Johannesburg')).toBeInTheDocument();
+    });
+
+    fireEvent.click(screen.getByText('Johannesburg'));
+
+    await waitFor(() =>
+      expect(screen.getByText('Humidity')).toBeInTheDocument()
+    );
+  });
+
+  test('search result should not be visible', async () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText(/search for a city/i);
+    fireEvent.change(input, { target: { value: 'burg' } });
+    await waitFor(() =>
+      expect(screen.queryByText('Johannesburg')).not.toBeInTheDocument()
+    );
+  });
 });
