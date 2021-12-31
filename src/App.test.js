ee2ef7f82db65fcd46071e5c054445b0b6a6b21f
Chris Macdonald <macdochris@gmail.com>
2021-12-31 11:26:43 +0000

    set up test config

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders favourites heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/your favourites/i);
+  expect(headingElement).toBeInTheDocument();
 });
